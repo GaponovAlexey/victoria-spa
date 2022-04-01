058da9bf3a206286f30d5124917dd690889147fb
Kekelberi <alexei.gaponov@gmail.com>
2022-04-01 20:00:06 +0300

    save

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import { Header } from '../components/homePage/Header'
 import { HomepageTop } from '../components/homePage/HomepageTop'
 import { HeaderImg } from '../components/homePage/HeaderImg'
@@ -9,16 +8,33 @@ import { ComentsPhoto } from '../components/coments/ComentsPhoto'
 import { Certificates } from '../components/Sertificats/Certificates'
 import { Contacts } from '../components/Contacts/Contacts'
 import { Map } from '../components/map/Map'
+import { css } from '@emotion/css'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div
+      id='home'
+      className={css`
+        width: 960px;
+        font-family: 'Raleway', sans-serif;
+        margin: 0 auto;
+        @media (max-width: 768px) {
+          max-width: 710px;
+        }
+        @media (max-width: 420px) {
+          max-width: 350px;
+        }
+      `}
+    >
       <Head>
         <title>Педикюр у Виктории</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' href='#!' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Raleway:wght@400;600&display=swap'
+          rel='stylesheet'
+        />
       </Head>
       <Header />
-
       <HomepageTop />
       <HeaderImg />
       <About />
