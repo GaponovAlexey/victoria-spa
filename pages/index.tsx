1ba9a4539afedb96c7459ca6804f49f61583fde9
Kekelberi <alexei.gaponov@gmail.com>
2022-04-01 22:00:39 +0300

    save

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,40 +10,43 @@ import { Contacts } from '../components/Contacts/Contacts'
 import { Map } from '../components/map/Map'
 import { css } from '@emotion/css'
 import useSWR from 'swr'
+import { Html } from 'next/document'
 
 const Home: NextPage = () => {
   return (
-    <div
-      id='home'
-      className={css`
-        width: 960px;
-        font-family: 'Raleway', sans-serif;
-        margin: 0 auto;
-        @media (max-width: 768px) {
-          max-width: 710px;
-        }
-        @media (max-width: 420px) {
-          max-width: 350px;
-        }
-      `}
-    >
-      <Head>
-        <title>Педикюр у Виктории</title>
-        <link rel='icon' href='#!' />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Raleway:wght@400;600&display=swap'
-          rel='stylesheet'
-        />
-      </Head>
-      <Header />
-      <HomepageTop />
-      <HeaderImg />
-      <About />
-      <ComentsPhoto />
-      <Certificates />
-      <Map />
-      <Contacts />
-    </div>
+    <Html>
+      <div
+        id='home'
+        className={css`
+          width: 960px;
+          font-family: 'Raleway', sans-serif;
+          margin: 0 auto;
+          @media (max-width: 768px) {
+            max-width: 710px;
+          }
+          @media (max-width: 420px) {
+            max-width: 350px;
+          }
+        `}
+      >
+        <Head>
+          <title>Педикюр у Виктории</title>
+          <link rel='icon' href='#!' />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Raleway:wght@400;600&display=swap'
+            rel='stylesheet'
+          />
+        </Head>
+        <Header />
+        <HomepageTop />
+        <HeaderImg />
+        <About />
+        <ComentsPhoto />
+        <Certificates />
+        <Map />
+        <Contacts />
+      </div>
+    </Html>
   )
 }
 
